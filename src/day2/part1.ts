6ffc39531e4b28686962332ecb103616e917188d
Ola Petersson <ola.petersson@meltwater.com>
2023-12-06 09:59:59 +0100

    wat

--- src/day2/part1.ts
+++ src/day2/part1.ts
@@ -1,7 +1,6 @@
 //const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
 const input: string = await Bun.file(`${import.meta.dir}/example`).text();
 
-const numberPattern = /\d+/g;
 // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 class Game {
   id: number
@@ -12,40 +11,43 @@ class Game {
   constructor(line: string) {
     const firstSplit = line.split(':')
     this.id = +firstSplit[0].split(' ')[1]
-    // the rest includes 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     firstSplit[1].split(';').forEach(sets => {
-      // 3 blue, 4 red;
       sets.split(',').forEach(colorBox => {
-        const splitted = removeWhitespace(colorBox).split(' ')
-        const [color, fest] = removeWhitespace(colorBox).split(' ')
-        console.log({test, fest})
-        console.log({splitted})
-        const color = splitted[0].trim()
-        const amount = splitted[1].trim()
-//        console.log('colorBox', {colorBox, color})
+        const [amount, color] = removeWhitespace(colorBox).split(' ')
         switch (color.trim()) {
           case "red":
-            this.reds += +amount.trim()
+            this.reds = Math.max(+amount.trim(), this.reds)
+            break;
           case "blue":
-            this.blues += +amount.trim()
+            this.blues = Math.max(+amount.trim(), this.blues)
+            break;
           case "green":
-            this.greens += +amount.trim()
+            this.greens = Math.max(+amount.trim(), this.greens)
+            break;
         }
       })
     })
+  }
+
+  isPossible = () => {
+    return this.blues <= 14 && this.reds <= 12 && this.greens <= 13
+
   }
 }
 
 const removeWhitespace = (value: string) => value.replace(/^\s+|\s+$/g, '');
 
 const lines = input
-    .split('\n')
-    .filter( it => it !== '' && it !== null)
+  .split('\n')
+  .filter(it => it !== '' && it !== null)
 
-const result = lines.map(line => {
-  new Game(line)
-})
-console.log({result})
+const result = lines
+  .map(line => new Game(line))
+//.filter(game => game.isPossible())
+
+//.reduce((acc, current) => acc + current.id, 0)
+
+console.log({ result })
 /*.map(line => line.match(numberPattern)?.join('') ?? '0')
   .map(linenumbers => number(linenumbers[0] + linenumbers[linenumbers.length - 1]))
   .reduce((acc, current) => acc + current, 0)
