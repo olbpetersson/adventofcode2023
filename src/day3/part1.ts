1157174ad0ffad07c9e91d2f29219fe1b232b7f6
Ola Petersson <ola.petersson@meltwater.com>
2023-12-06 20:56:16 +0100

    day3 part1

--- src/day3/part1.ts
+++ src/day3/part1.ts
@@ -2,15 +2,14 @@ const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
 //const input: string = await Bun.file(`${import.meta.dir}/example`).text();
 
 const digitRegex = /^[0-9]$/
-const removeWhitespace = (value: string) => value.replace(/^\s+|\s+$/g, '');
 
 const lines = input
   .split('\n')
   .filter(it => it !== '' && it !== null)
 
 const hasAdjacentMarker = (widthMarker: number, heightMarker: number, matrix: string[][]) => {
-  for (let width = Math.max(0, widthMarker - 1); width <= Math.min(matrix[0].length, widthMarker + 1); width++) {
-    for (let height = Math.max(0, heightMarker - 1); height <= Math.min(matrix.length-1, heightMarker + 1); height++) {
+  for (let width = Math.max(0, widthMarker - 1); width <= Math.min(matrix[0].length - 1, widthMarker + 1); width++) {
+    for (let height = Math.max(0, heightMarker - 1); height <= Math.min(matrix.length - 1, heightMarker + 1); height++) {
       const target = matrix[height][width]
       if (!(digitRegex.test(target) || target === '.')) {
         return true
@@ -22,6 +21,7 @@ const hasAdjacentMarker = (widthMarker: number, heightMarker: number, matrix: st
 class NumberWithAdjacentMarker {
   numberString: string = '';
   hasAdjacentMarker: boolean = false
+  lastIndex: number[] = []
 }
 
 let currentNumber = new NumberWithAdjacentMarker()
@@ -33,10 +33,14 @@ matrix.forEach((line, i) => {
   line.forEach((value, k) => {
     if (digitRegex.test(value)) {
       currentNumber.numberString += value
+      if (currentNumber.numberString === '111') {
+        console.log({ currentNumber })
+      }
       const hasMarker = hasAdjacentMarker(k, i, matrix)
       if (hasMarker) {
         currentNumber.hasAdjacentMarker = hasMarker
       }
+      currentNumber.lastIndex = [k, i]
     } else {
       if (currentNumber.hasAdjacentMarker) {
         candidates.push(currentNumber)
