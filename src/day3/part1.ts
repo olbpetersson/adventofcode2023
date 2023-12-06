8e3d3ed8724cb4eeee67022ef3f96765ed01aa31
Ola Petersson <ola.petersson@meltwater.com>
2023-12-06 16:48:44 +0100

    day3 hath beguuuun

---
+++ src/day3/part1.ts
@@ -0,0 +1,53 @@
+const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
+//const input: string = await Bun.file(`${import.meta.dir}/example`).text();
+
+const digitRegex = /^[0-9]$/
+const removeWhitespace = (value: string) => value.replace(/^\s+|\s+$/g, '');
+
+const lines = input
+  .split('\n')
+  .filter(it => it !== '' && it !== null)
+
+const hasAdjacentMarker = (widthMarker: number, heightMarker: number, matrix: string[][]) => {
+  for (let width = Math.max(0, widthMarker - 1); width <= Math.min(matrix[0].length, widthMarker + 1); width++) {
+    for (let height = Math.max(0, heightMarker - 1); height <= Math.min(matrix.length-1, heightMarker + 1); height++) {
+      const target = matrix[height][width]
+      if (!(digitRegex.test(target) || target === '.')) {
+        return true
+      }
+    }
+  }
+}
+
+class NumberWithAdjacentMarker {
+  numberString: string = '';
+  hasAdjacentMarker: boolean = false
+}
+
+let currentNumber = new NumberWithAdjacentMarker()
+const candidates: NumberWithAdjacentMarker[] = []
+const matrix = lines
+  .map(word => word.split(''))
+
+matrix.forEach((line, i) => {
+  line.forEach((value, k) => {
+    if (digitRegex.test(value)) {
+      currentNumber.numberString += value
+      const hasMarker = hasAdjacentMarker(k, i, matrix)
+      if (hasMarker) {
+        currentNumber.hasAdjacentMarker = hasMarker
+      }
+    } else {
+      if (currentNumber.hasAdjacentMarker) {
+        candidates.push(currentNumber)
+      }
+      currentNumber = new NumberWithAdjacentMarker()
+    }
+  })
+})
+
+
+const result = candidates.reduce((acc, current) =>
+  acc + +current.numberString, 0)
+console.log({ result })
+
