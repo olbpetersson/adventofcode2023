51a4c80a2c7ee587c970260baf0776f614065533
Ola Petersson <ola.petersson@meltwater.com>
2023-12-07 09:28:57 +0100

    day3 part2

--- src/day3/part2.ts
+++ src/day3/part2.ts
@@ -7,12 +7,12 @@ const lines = input
   .split('\n')
   .filter(it => it !== '' && it !== null)
 
-const hasAdjacentMarker = (widthMarker: number, heightMarker: number, matrix: string[][]) => {
+const getMarkerCoordinates = (widthMarker: number, heightMarker: number, matrix: string[][]) => {
   for (let width = Math.max(0, widthMarker - 1); width <= Math.min(matrix[0].length - 1, widthMarker + 1); width++) {
     for (let height = Math.max(0, heightMarker - 1); height <= Math.min(matrix.length - 1, heightMarker + 1); height++) {
       const target = matrix[height][width]
-      if (!(digitRegex.test(target) || target === '.')) {
-        return true
+      if (target === '*') {
+        return `${height}${width}`
       }
     }
   }
@@ -20,11 +20,12 @@ const hasAdjacentMarker = (widthMarker: number, heightMarker: number, matrix: st
 
 class NumberWithAdjacentMarker {
   numberString: string = '';
-  hasAdjacentMarker: boolean = false
+  markerCoordinates: string = ''
 }
 
 let currentNumber = new NumberWithAdjacentMarker()
-const candidates: NumberWithAdjacentMarker[] = []
+const asteriskMap: Map<string, string[]> = new Map<string, string[]>()
+
 const matrix = lines
   .map(word => word.split(''))
 
@@ -32,21 +33,25 @@ matrix.forEach((line, i) => {
   line.forEach((value, k) => {
     if (digitRegex.test(value)) {
       currentNumber.numberString += value
-      const hasMarker = hasAdjacentMarker(k, i, matrix)
-      if (hasMarker) {
-        currentNumber.hasAdjacentMarker = hasMarker
+      const markerCoordinates = getMarkerCoordinates(k, i, matrix)
+      if (markerCoordinates) {
+        currentNumber.markerCoordinates = markerCoordinates
       }
     } else {
-      if (currentNumber.hasAdjacentMarker) {
-        candidates.push(currentNumber)
+      if (currentNumber.markerCoordinates) {
+        const prevValue = asteriskMap.get(currentNumber.markerCoordinates)
+        const newValue = (prevValue ? [...prevValue, currentNumber.numberString] : [currentNumber.numberString])
+        asteriskMap.set(currentNumber.markerCoordinates, newValue)
       }
       currentNumber = new NumberWithAdjacentMarker()
     }
   })
 })
 
+const twoMatchesSelection =
+  [...asteriskMap.values()].filter(it => it.length == 2)
 
-const result = candidates.reduce((acc, current) =>
-  acc + +current.numberString, 0)
+const result = twoMatchesSelection.reduce((acc, current) =>
+  acc + +current[0] * +current[1], 0)
 console.log({ result })
 
