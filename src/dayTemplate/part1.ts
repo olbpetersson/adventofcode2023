9b98c7ecfe011d4d9899f848d1e1da660a767b52
Ola Petersson <ola.petersson@meltwater.com>
2023-12-07 09:46:33 +0100

    templates

---
+++ src/dayTemplate/part1.ts
@@ -0,0 +1,11 @@
+const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
+//const input: string = await Bun.file(`${import.meta.dir}/example`).text();
+
+const lines = input
+  .split('\n')
+  .filter(it => it !== '' && it !== null)
+
+const result = lines.reduce((acc, current) =>
+  acc + +current, 0)
+console.log({ result })
+
