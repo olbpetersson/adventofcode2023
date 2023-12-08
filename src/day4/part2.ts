d9127dc381c611a3aae67820f381489882eb6eed
Ola Petersson <ola.petersson@meltwater.com>
2023-12-08 09:32:34 +0100

    day4 part2

--- src/day4/part2.ts
+++ src/day4/part2.ts
@@ -1,43 +1,43 @@
-//const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
-const input: string = await Bun.file(`${import.meta.dir}/example`).text();
+const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
+const exampleInput: string = await Bun.file(`${import.meta.dir}/example`).text();
+
 const funkyMap = new Map<number, number>()
 
-const updateMap = (id: number, steps: number) => {
-  for (let i = id+1; i <= id+steps; i++) {
-    const oldValue = funkyMap.get(id)
-    const newValue = oldValue ? (oldValue + 1) * oldValue : 1
-    funkyMap.set(i, newValue)
+const updateWinningCopies = (id: number, steps: number) => {
+  const numerate = funkyMap.get(id) || 1
+  for (let h = 0; h < numerate; h++) {
+    for (let i = id + 1; i <= id + steps; i++) {
+      funkyMap.set(i, (funkyMap.get(i) || 1) + 1)
+    }
   }
-  console.log('After card', { id, funkyMap })
 }
 // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 class ScratchCard {
   winningNumbers: number[]
   yourNumbers: number[]
-  winningScore: number
 
   constructor(line: string) {
     const split = line.split(':')
-    const gameId = +split[0].split(' ')[1]
+    const gameId = +split[0].split(/\s+/)[1]
+    funkyMap.set(gameId, funkyMap.get(gameId) || 1)
     const [winningRaw, yourRaw] = split[1].split('|')
-    this.winningNumbers = winningRaw.split(' ')
+    this.winningNumbers = winningRaw.split(/\s+/)
       .filter(it => it !== '')
       .map(it => +it)
-    this.yourNumbers = yourRaw.split(' ')
+    this.yourNumbers = yourRaw.split(/\s+/)
       .filter(it => it !== '')
       .map(it => +it)
-    const sharedNumbers = this.winningNumbers.filter(win => this.yourNumbers.indexOf(win) > -1)
-    this.winningScore = Math.floor(Math.pow(2, sharedNumbers.length - 1))
-    updateMap(gameId, sharedNumbers.length)
+    const sharedNumbers = this.winningNumbers.filter(win => this.yourNumbers.includes(win))
+    updateWinningCopies(gameId, sharedNumbers.length)
   }
 }
 
-const lines = input
+input
   .split('\n')
   .filter(it => it !== '' && it !== null)
   .map(line => new ScratchCard(line))
 
-const result = lines.reduce((acc, current) =>
-  acc + current.winningScore, 0)
+const result = [...funkyMap.values()].reduce((acc, current) =>
+  acc + current, 0)
 console.log({ result, })
 
