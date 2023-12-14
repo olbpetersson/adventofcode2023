bf2cfe157709899e2c810a96302e7eaf5fde1367
Ola Petersson <ola.petersson@meltwater.com>
2023-12-14 20:08:45 +0100

    day9 wip

---
+++ src/day9/part1.ts
@@ -0,0 +1,29 @@
+const input: string = await Bun.file(`${import.meta.dir}/input1`).text();
+const exampleInput: string = await Bun.file(`${import.meta.dir}/example`).text();
+
+const parseLine = (line: string) => {
+  let numbers = line.split(' ').map(v => +v)
+  console.log('hello', numbers)
+  const finalPos = [numbers[numbers.length - 1]]
+  let diffTables
+  do {
+    diffTables = []
+    for (let i = 0; i < numbers.length - 1; i++) {
+      diffTables[i] = numbers[i + 1] - numbers[i]
+    }
+    finalPos.push(diffTables[diffTables.length - 1])
+    numbers = diffTables
+  } while (diffTables.some(v => v !== 0))
+
+  return finalPos.reduce((acc, current) => acc + current, 0)
+}
+
+const lines = exampleInput
+  .split('\n')
+  .filter(it => it !== '' && it !== null)
+  .map(parseLine)
+
+const result = lines.reduce((acc, current) =>
+  acc + +current, 0)
+console.log({ lines })
+
